e52344fc3f39a0f827c131a878f505accdf31bcd
Petteri Havia <petsku15@hotmail.fi>
2023-08-28 04:46:34 +0300

    Routing using useParams and id + conditional rendering and filtering of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,10 +10,12 @@ import {
   CardInfo,
   CustomColorSVG,
   StyledTab,
+  ClientProjectGrid
 } from "../styles/Elements.style";
 import row from "../assets/row.svg";
 import { ProjectArray } from "./ProjectsArray";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   const [toggleIndex, setToggleIndex] = useState(1);
@@ -21,7 +23,9 @@ const Projects = () => {
   const ProjectCard = ({ item }) => {
     return (
       <Card>
-        <img src={item.img} alt={item.project_name} />
+        <Link to={`projects/${item.id}`}>
+          <img src={item.img} alt={item.project_name} />
+        </Link>
         <CardInfo>
           <h3>{item.project_name}</h3>
           <h3>{item.framework}</h3>
@@ -29,6 +33,11 @@ const Projects = () => {
       </Card>
     );
   };
+
+  const personalProjects = ProjectArray.filter(
+    (item) => item.type === "Personal"
+  );
+  const clientProjects = ProjectArray.filter((item) => item.type === "Client");
 
   const toggleTab = (index) => {
     setToggleIndex(index);
@@ -42,24 +51,51 @@ const Projects = () => {
           <ProjectHeading>
             <PageHeading>
               <h2>Projects</h2>
-              <CustomColorSVG src={row} alt="circle row" />
+              <CustomColorSVG src={row} alt="circle illustration" />
             </PageHeading>
             <TabHeading>
-              <StyledTab $isActive={toggleIndex === 1} onClick={() => toggleTab(1)}>
+              <StyledTab
+                $isActive={toggleIndex === 1}
+                onClick={() => toggleTab(1)}
+              >
                 <h3>ALL</h3>
               </StyledTab>
-              <StyledTab $isActive={toggleIndex === 2} onClick={() => toggleTab(2)}>
+              <StyledTab
+                $isActive={toggleIndex === 2}
+                onClick={() => toggleTab(2)}
+              >
                 <h3>PERSONAL</h3>
               </StyledTab>
-              <StyledTab $isActive={toggleIndex === 3} onClick={() => toggleTab(3)}>
+              <StyledTab
+                $isActive={toggleIndex === 3}
+                onClick={() => toggleTab(3)}
+              >
                 <h3>CLIENT</h3>
               </StyledTab>
             </TabHeading>
           </ProjectHeading>
-          <ProjectGrid $isActive={toggleIndex === 1}>
-            {ProjectArray.map((item, key) => (
-              <ProjectCard key={item.id} item={item} />
-            ))}
+          <ProjectGrid>
+            {toggleIndex === 1 && (
+              <>
+                {ProjectArray.map((item, key) => (
+                  <ProjectCard key={item.id} item={item} />
+                ))}
+              </>
+            )}
+            {toggleIndex === 2 && (
+              <>
+                {personalProjects.map((item, key) => (
+                  <ProjectCard key={item.id} item={item} />
+                ))}
+              </>
+            )}
+            {toggleIndex === 3 && (
+              <ClientProjectGrid>
+                {clientProjects.map((item, key) => (
+                  <ProjectCard key={item.id} item={item} />
+                ))}
+              </ClientProjectGrid>
+            )}
           </ProjectGrid>
         </InnerProjectContainer>
       </ProjectContainer>
