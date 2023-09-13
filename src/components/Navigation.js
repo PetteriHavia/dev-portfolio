8615214ada81b9fecde904a153c86bcb04224003
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-09-13 06:03:45 +0300

    Add working mobile menu and styled router link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,17 @@
 import {
   Container,
   InnerContainer,
-  NavLinks,
+  NavLinkList,
   NavLogo,
   NavigationContainer,
+  StyledLink,
 } from "../styles/Elements.style";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import {FiMenu, FiX} from 'react-icons/fi';
 
 const Navigation = ({aboutRef, projectRef, homeRef}) => {
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const scrollToSection = (ref) => {
     if (ref && ref.current) {
@@ -15,18 +19,23 @@ const Navigation = ({aboutRef, projectRef, homeRef}) => {
     }
   };
 
+  const toggleMenu = () => {
+    {isOpen ? setIsOpen(false) : setIsOpen(true)}
+  }
+
   return (
     <NavigationContainer>
       <InnerContainer>
         <NavLogo>
           <h1>Logo</h1>
         </NavLogo>
-        <NavLinks>
-          <Link to="/#home" onClick={() => scrollToSection(homeRef)}><p>&gt; HOME</p></Link>
-          <Link to="/#about" onClick={() => scrollToSection(aboutRef)}><p>&gt; ABOUT</p></Link>
-          <Link to="/#projects" onClick={() => scrollToSection(projectRef)}><p>&gt; PROJECTS</p></Link>
-          <Link to="/#contact"><p>&gt; CONTACT</p></Link>
-        </NavLinks>
+        <NavLinkList $isOpen={isOpen} className = {isOpen ? "mobileMenu" : ""}>
+          <StyledLink to="/#home" onClick={() => scrollToSection(homeRef)}>&gt; HOME</StyledLink>
+          <StyledLink to="/#about" onClick={() => scrollToSection(aboutRef)}>&gt; ABOUT</StyledLink>
+          <StyledLink to="/#projects" onClick={() => scrollToSection(projectRef)}>&gt; PROJECTS</StyledLink>
+          <StyledLink to="/#contact">&gt; CONTACT</StyledLink>
+        </NavLinkList>
+        {isOpen ? <FiX onClick={toggleMenu}/> : <FiMenu onClick={toggleMenu}/>}
       </InnerContainer>
     </NavigationContainer>
   );
