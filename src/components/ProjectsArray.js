f451a29b593d853020ac63f3d1b5ccd46d7dee9f
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-09-13 06:05:14 +0300

    Changed project-info texts

--- src/components/ProjectsArray.js
+++ src/components/ProjectsArray.js
@@ -11,8 +11,8 @@ export const ProjectArray = [
         framework: "React",
         img: task_app_img,
         project_info: [
-            "This project is a React front-end interface for the public API knows as PokeAPI that contains information about the pokemon series and games. The idea was to create a Pokédex-style application. This app allows users to search for specific Pokémon and view relevant information about them.",
-            "The search functionality within the app permits users to look up individual Pokémon using their names or unique ID numbers. Additionally, users have the option to select a particular generation from a dropdown menu (such as Kanto, Johto, Kalos, etc.). Upon selecting a generation, the app displays a list of all the Pokémon belonging to that specific generation.",
+            "I created this simple to-do app using React as a learning exercise. This app allows users to jot down tasks and add them to a list. The list is divided into three categories: All, Active, and Completed. Users can mark tasks as completed by clicking the icon on the left side of each task, which moves them to the Completed category. Initially, users can also delete tasks by clicking the X icon.",
+            "At the bottom of the app, there's a section displaying the total number of active to-dos, helping users keep track of their pending tasks. Additionally, there's a Clear Completed button that allows users to remove all completed tasks from the list in one go.",
         ],
         github_link: "GitHub_link",
         url_link: "url link",
@@ -49,8 +49,8 @@ export const ProjectArray = [
         framework: "React",
         img: musicapp_img,
         project_info: [
-            "This project is a React front-end interface for the public API knows as PokeAPI that contains information about the pokemon series and games. The idea was to create a Pokédex-style application. This app allows users to search for specific Pokémon and view relevant information about them.",
-            "The search functionality within the app permits users to look up individual Pokémon using their names or unique ID numbers. Additionally, users have the option to select a particular generation from a dropdown menu (such as Kanto, Johto, Kalos, etc.). Upon selecting a generation, the app displays a list of all the Pokémon belonging to that specific generation.",
+            "Music player app made by using React, featuring a small library of songs for users to choose from. The app allows users to play, skip forward, and skip backward through the songs in the library. To access the library, users can click on the top right corner.",
+            "The library contains all the songs that the app currently has. Users can conveniently filter through the songs by typing the song's name into the input field. The styling of this app was accomplished using SASS, marking my first experience learning and using it",
         ],
         github_link: "GitHub_link",
         url_link: "url link",
@@ -68,9 +68,9 @@ export const ProjectArray = [
         framework: "WordPress",
         img: kravmagaveikkola_img,
         project_info: [
-            "This project is a React front-end interface for the public API knows as PokeAPI that contains information about the pokemon series and games. The idea was to create a Pokédex-style application. This app allows users to search for specific Pokémon and view relevant information about them.",
-            "The search functionality within the app permits users to look up individual Pokémon using their names or unique ID numbers. Additionally, users have the option to select a particular generation from a dropdown menu (such as Kanto, Johto, Kalos, etc.). Upon selecting a generation, the app displays a list of all the Pokémon belonging to that specific generation.",
-            "React Router was implemented in the project to enable dynamic navigation. Styling of the application was achieved using Styled Components, which was a new tool for me in this project. This allowed for a streamlined and modular approach to styling."
+            "I created a website for Krav Maga Veikkola, a self-defense training company. The company had a clear vision for the site, which needed to be simple and concise, containing essential information for both new and existing club members. To streamline ongoing maintenance and updates, the company opted for WordPress as the platform of choice.",
+            "WordPress was selected because it offers a user-friendly content management system that allows the company to easily make adjustments and perform simple changes without requiring any prior coding knowledge. To enhance the site's versatility and customization, we integrated the Elementor page builder plugin. With Elementor, the company can effortlessly update images, text, and even create new layouts for fresh content.",
+            "By leveraging WordPress and Elementor, Krav Maga Veikkola has a website that not only meets their immediate needs but also empowers them to adapt and evolve their online presence in the future."
         ],
         github_link: "GitHub_link",
         url_link: "url link",
@@ -87,7 +87,9 @@ export const ProjectArray = [
         framework: "WordPress",
         img: zerodistance_img,
         project_info: [
-            "Project Info text",
+            "Zero Distance Solutions is a Finnish-based training company with expertise in military, tactical, and security forces. The company embarked on the journey of creating a new website and opted for the popular WordPress platform.",
+            "The company provided essential materials for the website and also presented preliminary layout sketches for the pages they envisioned. Recognizing the importance of future content updates and ease of management, Zero Distance Solutions decided to integrate the Elementor page builder plugin.",
+            "Elementor was chosen for its user-friendly yet powerful editor, which facilitates easy visual design and component building through a simple drag-and-drop interface. This choice ensures that the company can effortlessly modify and enhance the website's content, allowing for seamless updates as their needs evolve over time.",
         ],
         github_link: "GitHub_link",
         url_link: "url link",
