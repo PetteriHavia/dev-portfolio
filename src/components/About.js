35ad336df92db845f0925c81cd2531b94f2b665b
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-09-13 06:00:56 +0300

    changed text

--- src/components/About.js
+++ src/components/About.js
@@ -2,33 +2,38 @@ import {
   Container,
   Column,
   InnerContainerAbout,
-  PageHeading
+  PageHeading,
 } from "../styles/Elements.style";
 import Row from "../assets/row.svg";
 import coding from "../assets/desktop-illustration-hero.svg";
 
-
-const About = ({aboutSectionRef}) => {
+const About = ({ aboutSectionRef }) => {
   return (
-      <Container id="about" ref={aboutSectionRef}>
-        <InnerContainerAbout>
-          <Column>
-            <PageHeading>
-              <h2>About</h2>
-              <img src={Row} alt="circle illustration" />
-            </PageHeading>
-            <p>
-              Hey, my name is Petteri Havia, a recent graduate from Laurea's
-              Bachelor's Degree Programme in Business Information Technology. I
-              have passion and interest in web development especially front-end.
-              The dynamic world of web development keeps me inspired and in the
-              future i wanna keep on learning and challenging myself by creating
-              interesting websites and applications.
-            </p>
-          </Column>
-            <img className="illustration-pc" src={coding } alt="coding" />
-        </InnerContainerAbout>
-      </Container>
+    <Container id="about" ref={aboutSectionRef}>
+      <InnerContainerAbout>
+        <Column>
+          <PageHeading>
+            <h2>About</h2>
+            <img src={Row} alt="circle illustration" />
+          </PageHeading>
+          <p>
+            I am Petteri Havia, and I have recently completed my Bachelor's Degree
+            Program in Business Information Technology. My studies primarily
+            focused on the captivating realm of web development, with a
+            particular interest in front-end. I enjoy creating
+            new lines of code and have a strong desire for learning, and the field of web
+            development offers plenty of opportunities for continuous growth
+            and exploration.
+          </p>
+          <p>
+            Apart from coding, I also have hobbies like gaming, maintaining
+            an active lifestyle through exercise, and enjoying a wide variety of
+            music.
+          </p>
+        </Column>
+        <img className="illustration-pc" src={coding} alt="coding" />
+      </InnerContainerAbout>
+    </Container>
   );
 };
 
