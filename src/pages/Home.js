51f858817496ec8d922179a5086ddebf11134ae6
Petteri Havia <petsku15@hotmail.fi>
2023-09-15 04:13:33 +0300

    Restructuring the components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,16 @@
 import HeroSection from "../components/HeroSection";
-import Navigation from "../components/Navigation";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Skills from "../components/SkillSection";
 import Footer from "../components/Footer";
-import { useRef } from "react";
 
-const Home = () => {
-
-  const homeRef = useRef(null);
-  const aboutRef = useRef(null);
-  const projectRef = useRef(null);
-
+const Home = ({ aboutRef, homeRef, projectRef }) => {
   return (
     <>
-      <Navigation aboutRef={aboutRef} projectRef={projectRef} homeRef={homeRef}/>
-      <HeroSection homeRef={homeRef}/>
-      <About aboutSectionRef={aboutRef}/>
+      <HeroSection homeRef={homeRef} />
+      <About aboutSectionRef={aboutRef} />
       <Skills />
-      <Projects projectSectionRef={projectRef}/>
+      <Projects projectSectionRef={projectRef} />
       <Footer />
     </>
   );
