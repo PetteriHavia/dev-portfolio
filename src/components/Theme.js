f972814c5bf9de33f3b5ce92b31b84533039f77c
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-09-15 04:16:51 +0300

    update

---
+++ src/components/Theme.js
@@ -0,0 +1,30 @@
+export const lightTheme = {
+  body: "#FFF",
+  text: "#363537",
+  background: "#363537",
+  color: {
+    h1_heading: "#130F49;",
+    h2_heading: "#130F49",
+    p_text: "#333",
+    git_icon_color: "#383873;",
+    nav_links: "#383873",
+  },
+  menu_box_shadow: "0px 0px 7px 2px #d9d9d9cc",
+  nav_background: "#fff",
+  project_card_shadow: "0px 0px 7px 2px #d9d9d9cc",
+};
+
+export const darkTheme = {
+  body: "#363537",
+  text: "#FAFAFA",
+  background: "#999",
+  color: {
+    h1_heading: "#fff",
+    p_text: "#fff",
+    git_icon_color: "#fff;",
+    nav_links: "#fff",
+  },
+  box_shadow: "0px 0px 2px 2px #d9d9d9cc",
+  nav_background: "#363537",
+  project_card_shadow: "0px 0px 5px 0px #d9d9d9cc",
+};
